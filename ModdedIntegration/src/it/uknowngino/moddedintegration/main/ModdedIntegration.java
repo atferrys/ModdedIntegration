96093ff0036bd034d7a22a390ba458f24b4d37f8
UknownGino <61624086+UknownGino@users.noreply.github.com>
2021-04-03 20:37:01 +0200

    Initial Commit

---
+++ ModdedIntegration/src/it/uknowngino/moddedintegration/main/ModdedIntegration.java
@@ -0,0 +1,43 @@
+package it.uknowngino.moddedintegration.main;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import it.uknowngino.moddedintegration.functions.IntegrationUtils;
+
+public class ModdedIntegration extends JavaPlugin {
+	
+	static ModdedIntegration plugin;
+	
+	public void onEnable() {
+		
+		plugin = this;
+		
+		if(Bukkit.getPluginManager().isPluginEnabled("Essentials")) {
+			
+			IntegrationUtils.populateCSV();
+			
+		} else {
+			
+			System.out.println("ModdedIntegration » EssentialsX not found!");
+			Bukkit.getPluginManager().disablePlugin(this);
+			
+		}
+		
+		System.out.println("ModdedIntegration » Plugin enabled!");
+
+	}
+	
+	public void onDisable() {
+		
+		System.out.println("ModdedIntegration » Plugin disabled!");
+		
+	}
+	
+	public static ModdedIntegration getInstance() {
+		
+		return plugin;
+		
+	}
+
+}
